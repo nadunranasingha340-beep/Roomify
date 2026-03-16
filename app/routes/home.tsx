36399c74b830d14f3dbadc768bcf94d5f43d7056
Nadun Ranasingha <nadunranasingha340@gmail.com>
2026-03-16 17:59:41 +0530

    implment visualizer and export

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -105,7 +105,7 @@ export default function Home() {
                             </div>
 
                             <h3>Upload your floor plan</h3>
-                            <p>Supports JPG, PNG, formats up to 10MB</p>
+                            <p>Supports JPG, PNG, WEBP formats up to 10MB</p>
                         </div>
 
                         <Upload onComplete={handleUploadComplete} />
